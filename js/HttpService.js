445695dbc71f4961ecabbc145912cb3916263557
unknown <421251064@qq.com>
2015-08-27 00:19:51 +0800

    20150827

---
+++ js/HttpService.js
@@ -0,0 +1,14 @@
+var mymodule = angular.module('MyModule',[]);
+mymodule.controller('LoadDataCtrl', ['$scope','$http', function($scope,$http){
+	$http({
+		method: 'GET',
+		url: 'data.json'
+	}).success(function(data, status, headers, config) {
+		console.log("success...");
+		console.log(data);
+		$scope.users = data;
+	}).error(function(data, status, headers, config) {
+		console.log("error...");
+	})
+}]);
+
